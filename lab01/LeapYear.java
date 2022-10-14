6a8694cc8fd4fd4adcfd498d9bc8a4c727fec9e7
Maksat Zhazbayev <maksat_zhazbaev@utexas.edu>
2022-10-14 16:48:24 +0500

    IntelliJ setup; modified LeapYear; added magic word

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -1,6 +1,6 @@
 /**
  * Class that determines whether or not a year is a leap year.
- * @author YOUR NAME HERE
+ * @author Maksat Jazbay
  */
 public class LeapYear {
 
@@ -9,6 +9,7 @@ public class LeapYear {
      */
     public static boolean isLeapYear(int year) {
         // Optional TODO: Fill in this method.
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
 
     /** Calls isLeapYear to print correct statement. */
