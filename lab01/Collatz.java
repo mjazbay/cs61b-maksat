28d7c99024147a28b73fdb25b4379c7fe90e9f5b
Maksat Zhazbay <35613358+mzhazbay@users.noreply.github.com>
2022-10-14 17:19:08 +0500

    finished collatz exercise (#1)
    squash and merge

--- lab01/Collatz.java
+++ lab01/Collatz.java
@@ -1,16 +1,25 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Maksat Jazbay
  */
 public class Collatz {
 
     /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
-        // TODO: Fill in this method.
-        return 1;
+        /*
+        If n is even, the next number is n/2. If n is odd, the next number is 3n+1.
+        If n is 1, the sequence is over.
+        * For example nextNumber(5) should return 16.
+        Since 5 is odd, the next number is 3×5+1=16.
+        Since 16 is even, the next number is 8. Since 8 is even, the next number is 4.
+        Since 4 is even the next number is 2. Since 2 is even, the next number is 1.
+        At that point we’re done. The sequence was 5,16,8,4,2,1.
+         * */
+        return n % 2 == 0 ? n / 2 : (3 * n) + 1;
     }
 
     public static void main(String[] args) {
         int n = 5;
+        System.out.print("Collatz sequence is --> ");
         System.out.print(n + " ");
 
         // Some starter code to test
